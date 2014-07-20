a7dfb86589af9f3921bdd5a938136dfe404280e7
David <davidabelman@gmail.com>
2014-07-20 09:42:20 +0100

    Separated out GA functionality and added page loaded event

---
+++ js/ga_helper.js
@@ -0,0 +1,31 @@
+// GA initiation code
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-52998648-1', 'auto');
+ga('send', 'pageview');
+current_page = window.document.title;
+ga('send', 'event', current_page, 'page loaded');
+
+// When doument is ready
+$(document).ready( function () {
+
+    /* Records specific portfolio opens */
+    $('.portfolio-item').click( 
+            function() {
+                var id = $(this).find('a').attr('id');
+                current_page = window.document.title;
+                ga('send', 'event', current_page, 'portfolio item opened', id);
+        }
+    );        
+
+    /* Records any link clicked on page*/
+    $('a').click( function() { 
+        link_location = $(this).attr('href');
+        current_page = window.document.title;
+        ga('send', 'event', current_page, 'link clicked', link_location);
+    } )
+
+});
